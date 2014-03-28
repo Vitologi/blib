452e09c30d56ce037f09fdd54714b16f929df966
Spektr <spektr1987@rambler.ru>
2014-03-28 17:18:36 +0400

    set config && store method, start outer include

--- b-/b-blib/b-blib.js
+++ b-/b-blib/b-blib.js
@@ -1,17 +1,16 @@
-(function(){
+(function( window ){
 	
-	/* PRIVATE VARIABLE */
+	/** PRIVATE VARIABLE */
     var config = {
+			'_private':{
+				'version':true,
+				'alias':true,
+				'ajax':true,
+				'store':true
+			},
             'version':'0.2.0',
-			'settings':{},
-			'events':{
-				'onclick':true,
-				'onmouseover':true,
-				'onmouseout':true,
-				'onfocus':true,
-				'onblur':true,
-				'onkeyup':true,
-				'onkeydown':true
+			'alias':{
+				'head': document.getElementsByTagName('head')[0] //под задачу
 			},
 			'ajax':{
 				'dataType':"text",
@@ -21,13 +20,15 @@
 				'url':"/",
 				'headers':["X-Requested-With","XMLHttpRequest"]
 			},
-			'alias':{
-				'head': document.getElementsByTagName('head')[0],
-				'body': document.getElementsByTagName('body')[0]
-			}
+			'store':{
+				'isset':('localStorage' in window && window['localStorage'] !== null)?true:false,
+				'data':{}
+			},
+			'system':{},
+			'user':{}
         };
 	
-	/* PRIVATE METHODS */
+	/** PRIVATE METHODS */
 	var Blib = function(){
             return new init(arguments);
         },
@@ -40,16 +41,33 @@
 
         },
 		clone = function(obj){
-			var temp = obj.cloneNode(false);
-			for(key in config.events){
-				if(key in obj){temp[key]=obj[key];};
+
+			if(typeof(obj) != 'object' || obj === null){
+				return obj;
 			}
 			
-			if(obj.hasChildNodes()){
-				var len=obj.childNodes.length;
-				for(var i=0;i<len;i++){
-					temp.appendChild(clone(obj.childNodes[i]));
+			//for DOM elements
+			if(obj.cloneNode){
+				var temp = obj.cloneNode(false);
+				
+				for(key in obj){
+					if(key.substr(0,2) == "on" && obj[key] != null) temp[key]=clone(obj[key]);
 				}
+				
+				if(obj.hasChildNodes()){
+					var len=obj.childNodes.length;
+					for(var i=0;i<len;i++){
+						temp.appendChild(clone(obj.childNodes[i]));
+					}
+				}
+				return temp;
+			}
+			
+			//for all OTHER
+			var temp = new obj.constructor();
+			for (i in obj){
+				if(temp[i] === obj[i]){continue;};
+				temp[i] = clone(obj[i]);
 			}
 			return temp;
 		},
@@ -102,17 +120,102 @@
 			}
 			
 			return elements;
+		},
+		navigate = function(obj, selector, value){
+			
+			if(!obj || typeof(selector) != 'string') return;
+			
+			var needle = (selector.indexOf('.')!=-1)?selector.split('.'):[selector],
+				i = 0,
+				len = needle.length
+				isValue = typeof(value) !== 'undefined';
+			
+			while(i<len){
+				if(isValue && i == len-1) break;
+				if(!(needle[i] in obj)) return;
+				obj = obj[needle[i]];
+				i++;
+			};
+
+			if(!isValue) return obj;
+			obj[needle[i]] = value;
+		},
+		storeSave = function(get){
+			if(!config.store.isset) return;
+			
+			if(get){
+				config.store.data =  JSON.parse(localStorage.getItem("blib")) || {};
+			}else{
+				localStorage.setItem("blib", JSON.stringify(config.store.data));
+			}
+		},
+		block2url = function(name, extension, path){
+			if(typeof(path) == "string") return path+name+"."+extension;
+			return name.substr(0,1)+"/"+name+"/"+name+"."+extension;
 		};
 	
 	
-	/* LIBRARY METHODS */
-	//set settings
-	Blib.settings = function(settings){
-		if(typeof settings === "object"){
-			config.settings = settings;
-		}
-		return this;
+	/** LIBRARY METHODS */
+	
+	Blib.block2url = block2url;
+	
+	/**
+	 * Method for work with blib configuration
+	 * 
+	 * @constructor
+	 * @param	{string}	option	- Path to configs option (use dots "." for dividing items)
+	 * @param	{multiple}	value	- Installed option
+	 * @return	{config}||{Blib}	- Returns the selected option or Blib for chaining
+	 */
+	Blib.config = function(option, value){
+		
+		if(typeof(option) != 'string') return clone(config);
+		
+		var result = config,
+			needle = (option.indexOf('.')!=-1)?option.split('.'):[option];
+			first = needle[0];
+			second =  needle[1] || false;
+		
+		if(typeof(value)  === 'undefined') return clone(navigate(result, option));
+		if(config["_private"][first] || (first == "_private" && !second && !(second in config["_private"]))) return Blib;
+		
+		navigate(result, option, value);
 	};
+	
+	Blib.store = {
+		'get':function(get){
+			if(typeof(navigate(config.store.data, get)) == 'undefined') storeSave('get');
+			var data = config.store.data;
+			if(typeof(get) != 'string')	return clone(data);
+			return clone(navigate(data, get));
+		},
+		'set':function(key, value){
+			if(typeof(key) == 'undefined')	config.store.data = {};
+			var data = config.store.data;
+			
+			if(key && typeof(value) == 'undefined'){
+				
+				if(key.indexOf('.')!=-1){
+					var needle = key.split('.'),
+						i=0,
+						len = needle.length;
+					
+					while(i<len){
+						key = needle[i];
+						if(i == len-1) break;
+						data = data[key];
+						i++;
+					}
+				}
+				
+				delete data[key];
+		
+			}else{
+				navigate(data, key, value);
+			}
+			storeSave();
+		}
+	}
 	
 	//set handler on DOM ready event
 	Blib.ready = function(handler){
@@ -218,9 +321,10 @@
 		
 	};
 	
-	/* OBJECT METHODS */
+	/** OBJECT METHODS */
 	Blib.prototype = {
 		'constructor':Blib,
+		'version': Blib.config('version'),
 		'length':0,
         'init':init,
 		
@@ -259,10 +363,6 @@
 			return this;
 		},
 		
-        'test':function(){
-            console.log(config);
-        },
-		
 		//get Array methods
         'push':[].push,
 		'sort':[].sort,
@@ -270,10 +370,242 @@
 	};
 	
 	
-	/* MAGIC */
-    init.prototype = Blib.prototype;
+	/** MAGIC */
+    init.prototype = Blib.fn = Blib.prototype;
 	if ( typeof window === "object" && typeof window.document === "object" ) {
 		window.blib = Blib;
 	}
+	
+	
+	
+})( window );
 
-})();
+/** захреначить find на основе getElement.call(obj, handle) */
+
+/** EXAMPLE FOR EXTEND LIBRARY & OBJECT METHODS */
+(function( Blib ){
+	//add object method
+	Blib.fn.test = function(){
+		console.log(this.length);
+		console.log(this);
+	}
+	
+	//add library method
+	Blib.test = function(){
+		console.log(this.length);
+		console.log(this);
+	}
+})(blib);
+
+
+/**
+ * Blib.include library. Allows get all script or style file in one. And store in local cache.
+ * 
+ */
+(function( Blib ){
+
+	var config = {
+		'item':true
+	}
+	Blib.config("include", config);
+	Blib.config("_private.include", true);
+	
+	/*
+	function(request){
+		
+		var store =  Blib.store,
+			isset = config.store.isset;
+			data = config.store.data;
+		
+		console.log(store.get());
+		
+	
+		//storeSave
+		
+		var operation = request['operation'],
+			type = request['type'],
+			fileName = request['fileName'],
+			fileCache = request['fileCache']||[],
+			file = request['file'],
+			obj = eval(type),
+			html = document.getElementById(fileName);
+		
+		if(operation=='clear'){
+			if(html){html.parentNode.removeChild(html);}
+			delete obj[fileName];			
+		}else if(operation=='set'){
+			if(!head.appendChild(file)){return alert("непрошло"+fileName);}
+			obj[fileName]={'version':version, 'list':fileCache};
+		}else if(operation=='getAllFiles'){
+			var arr=[];
+			for(key in obj){
+				if(obj[key]['list']){
+					arr = arr.concat(obj[key]['list']);
+				}else{
+					arr.push(key);
+				}
+			}
+			arr.sort();
+			var i = arr.length;
+			while (i--) {
+				if (arr[i] == arr[i-1]){
+					arr.splice(i, 1);
+				}
+			}
+			return arr;
+		}
+		
+		if(storageFlag){localStorage.setItem(type, JSON.stringify(obj));}
+		
+	};
+	*/
+	
+	/**
+	* include block (html+css+js in set container || css+js)
+	* @param {string} file		path of block without extension
+	* @param {string} target	selector where will be load block
+	*/
+	Blib.include =  function(file, target){
+		/*
+		cssFunction(file+'.css');
+
+		if(target){
+			var target = $(target);
+			$.ajax({
+				url:file+'.html?new='+version,
+				dataType: "html",
+				success: function(data){
+					for(key in target){
+						target[key].innerHTML=data;
+					}
+					jsFunction(file+'.js');
+				}
+			});
+		}else{
+			jsFunction(file+'.js');
+		}
+		*/
+	};
+	
+	/**
+	* include css file
+	* @param {string} cssFile	name of css file
+	* @param {string}[] inCache	files which contain in it
+	* @return {object}			this
+	*/
+	Blib.include.css = function(cssFile, inCache){
+		/*
+		cssFile = cssFile.toString();
+	
+		if((cssFile in css) && (css[cssFile]['version']==version)){ return this; }
+	
+		for(key in css){
+			var innerFiles = (css[key]['list'])?css[key]['list']:[];
+			for(var len=innerFiles.length, i=0;i<len;i++){
+				if(innerFiles[i]==cssFile){return cssFunction(key, innerFiles);}
+			}
+		}
+		
+		storageHandler({'operation':'clear', 'type':'css', 'fileName':cssFile});
+		var cssLink  = document.createElement('link');
+		cssLink.rel  = 'stylesheet';
+		cssLink.type = 'text/css';
+		cssLink.href = cssFile+"?new="+version;
+		cssLink.media = 'all';
+		cssLink.id = cssFile;
+		storageHandler({'operation':'set', 'type':'css', 'fileName':cssFile, 'fileCache':inCache, 'file':cssLink});
+		
+		return this;
+		*/
+	};
+	
+	/**
+	* include js file
+	* @param {string} jsFile	name of js file
+	* @param {string}[] inCache	files which contain in it
+	* @return {object}			this
+	*/
+	Blib.include.js = function(jsFile, inCache){
+		
+		/*
+		jsFile = jsFile.toString();
+
+		if(!(jsFile in js)){
+			for(key in js){
+				var innerFiles = (js[key]['list'])?js[key]['list']:[];
+				for(var i=0, len=innerFiles.length;i<len;i++){
+					if(innerFiles[i]==jsFile){return jsFunction(key, innerFiles);}
+				}
+			}
+		}
+		
+		storageHandler({'operation':'clear', 'type':'js', 'fileName':jsFile});
+		var scriptLink  = document.createElement('script');
+		scriptLink.id = jsFile;
+		scriptLink.src = jsFile+"?new="+version;
+		scriptLink.type="text/javascript";
+		storageHandler({'operation':'set', 'type':'js', 'fileName':jsFile, 'fileCache':inCache, 'file':scriptLink});
+		return this;
+		*/
+	};
+	
+	
+	
+	/**
+	* method for get version of site and load all stylesheet/script in one file
+	* @param {object} dataObject	object of setting
+	* {bool} script					for glue javascripts
+	* {srting}[]exception			blocks which will not be uploaded
+	* {srting}[]order				first turn load sctipts/if 'script' is false, then 'order' set chosen blocks
+	*/
+	Blib.include.fire = function(dataObject){
+		
+		/*
+		if(!dataObject['order'].length || dataObject['script']){
+			// first include all cache 
+			var arr = (storageFlag && localStorage.getItem('css'))?JSON.parse(localStorage.getItem('css')):{};
+			for(key in arr){
+				cssFunction(key, arr[key]['list']||[]);
+			}
+			var arr = (storageFlag && localStorage.getItem('js'))?JSON.parse(localStorage.getItem('js')):{};
+			for(key in arr){
+				jsFunction(key, arr[key]['list']||[]);
+			}
+		}
+		
+		// get files which we have 
+		var allCss = storageHandler({'operation':'getAllFiles', 'type':'css'}),
+			allJs = storageHandler({'operation':'getAllFiles', 'type':'js'}),
+			requestData = {
+				'version':version,
+				'data':{
+					'css':allCss,
+					'js':(dataObject['script']?allJs:"orderOnly")
+				},
+				'exception':(dataObject['exception'] || []),
+				'order':(dataObject['order'] ||["b-/b-blib-build/b-blib-build.js", "b-/b-jquery/b-jquery.js", "b-/b-jquery-ui/b-jquery-ui.js"])
+			};	
+
+		ready(function(){
+			$.ajax({
+				url:'b-/b-blib/b-blib.php',
+				data:requestData,
+				type:"DATA",
+				dataType: "json",
+				success: function(data){
+					if(!data['status']){return false;}
+					version = data['version'];
+					if(storageFlag){localStorage.setItem("version", JSON.stringify(version));}
+					if(data['css']){cssFunction(data['css']['name'], data['css']['list']);}
+					if(data['js']){jsFunction(data['js']['name'], data['js']['list']);}
+				}
+			});
+		});//ready
+		
+		*/
+	};
+	
+})( window.blib );
+
+
+

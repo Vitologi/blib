7d4533964cbb90ebba313f7130db9e6ff1e6ccfa
Spektr <spektr1987@rambler.ru>
2014-05-14 18:56:51 +0400

    set database for bConfig

---
+++ b/bConfig/__bDatabase/install.php
@@ -0,0 +1,19 @@
+<?php
+defined('_BLIB') or die();
+return array(
+	'create'	=> array(
+		'bConfig'	=> array(
+			'fields' => array(
+				'id'		=> array('type'=> 'INT(10) UNSIGNED', 'null'=>'NOT NULL', 'extra'=> 'AUTO_INCREMENT', 'comment'=>'Table for store configuration in JSON format'),
+				'name'		=> array('type'=> 'VARCHAR(45)', 'comment'=>'settings owner'),
+				'value'		=> array('type'=> 'VARCHAR(45)', 'comment'=>'owner identificator'),
+				'config'	=> array('type'=> 'TEXT', 'comment'=>'JSON serialized configurations'),
+				'bConfig_id'=> array('type'=> 'INT(10) UNSIGNED', 'comment'=>'id for default configurations'),
+			),
+			'primary'	=> array('id'),
+			'foreign'	=> array(
+				'bConfig_id'	=> null
+			)
+		)
+	)
+);

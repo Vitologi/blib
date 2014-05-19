22dfcb143516aa8985a5ea75944333f075288ed7
Spektr <spektr1987@rambler.ru>
2014-05-19 19:01:38 +0400

    add session date, storage path

--- b/bSession/__bDatabase/bSession__bDatabase_install.php
+++ b/bSession/__bDatabase/bSession__bDatabase_install.php
@@ -5,6 +5,7 @@ return array(
 		'bSession'	=> array(
 			'fields' => array(
 				'id'	=> array('type'=> 'CHAR(32)', 'null'=>'NOT NULL', 'comment'=>'Table for store session data in JSON format, as key use md5'),
+				'date'	=> array('type'=> 'TIMESTAMP', 'null'=>'NOT NULL', 'default'=>'CURRENT_TIMESTAMP', 'extra'=>'on update CURRENT_TIMESTAMP', 'comment'=>'Session date'),
 				'value'	=> array('type'=> 'TEXT', 'comment'=>'JSON serialized session data')
 			),
 			'primary'	=> array('id')
--- b/bSession/bSession.php
+++ b/bSession/bSession.php
@@ -4,6 +4,7 @@ defined('_BLIB') or die;
 class bSession extends bBlib{	
 	
 	private $id = false;
+	private $storePath = false;
 	private $data = array();
 	
 	protected function inputSelf(){
@@ -14,22 +15,24 @@ class bSession extends bBlib{
 	
 	protected function input($data, $caller){
 		$this->caller = get_class($caller);
-		$this->expire = 0;
-		$this->path = '/';
-		$this->domen = '';
-		$this->secure = false;
-		$this->httponly = false;
-		$this->sessionType = "database";
 	}
 	
 	
 	public function output(){
-		
+
 		if($this->_bSession){return array('bSession'=>$this->_bSession);}
-		
-		
+
 		switch($this->sessionType){
 			case "database":
+				
+				//defaults
+				$this->expire = 0;
+				$this->path = '/';
+				$this->domen = '';
+				$this->secure = false;
+				$this->httponly = false;
+				$this->sessionType = "database";
+				
 				if(isset($_COOKIE) && $_COOKIE['bSession']){
 					$Q = array(
 						'select'=>array(
@@ -67,8 +70,18 @@ class bSession extends bBlib{
 			
 			default:
 				if ( !isset($_SESSION) && !session_id() ) { 
+					
+					if(!$this->storePath){
+						$this->storePath = $this->getBlockPath().'/__storage';
+						if(!ini_set('session.save_path', $this->storePath)){
+							throw new Exception('Canot set php session save path');
+						};
+					}
+					
 					if(!session_start()){throw new Exception('Cannot use php session.');}; 
 				}
+				
+				
 				
 				$this->data = $_SESSION[__class__];
 				break;

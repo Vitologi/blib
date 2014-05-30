05dd4ff5a715f657e3734479b7368c3be49e77b2
Spektr <spektr1987@rambler.ru>
2014-05-30 11:50:14 +0400

    create one point (redirect on block bIndex)

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 	require_once("b/bBlib/bBlib.php");
 	bBlib::gate();
 	
-	
-	require_once("b/bIndex/bIndex.html");
+
 	$vremya_okonchaniya = microtime(true);
 	$vremya = $vremya_okonchaniya - $vremiya_starta;
 	//$vremya = round($vremya, 2);

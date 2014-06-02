edb20df2c794aac89ff6c33618411a41d01f910e
Spektr <spektr1987@rambler.ru>
2014-06-02 19:39:47 +0400

    start create bIndex

--- b/bIndex/__bDatabase/_install/bIndex__bDatabase_install.php
+++ b/bIndex/__bDatabase/_install/bIndex__bDatabase_install.php
@@ -5,15 +5,10 @@ return array(
 		'bIndex'	=> array(
 			'fields' => array(
 				'id'		=> array('type'=> 'INT(10) UNSIGNED', 'null'=>'NOT NULL', 'extra'=> 'AUTO_INCREMENT', 'comment'=>'Table for store page properties'),
-				'group'		=> array('type'=> 'VARCHAR(45)', 'comment'=>'settings owner'),
-				'name'		=> array('type'=> 'VARCHAR(45)', 'comment'=>'owner identificator'),
-				'value'	=> array('type'=> 'TEXT', 'comment'=>'JSON serialized configurations'),
-				'bConfig_id'=> array('type'=> 'INT(10) UNSIGNED', 'comment'=>'id for default configurations'),
+				'template'	=> array('type'=> 'VARCHAR(45)', 'null'=>'NOT NULL', 'comment'=>'template name = address'),
+				'meta'		=> array('type'=> 'TEXT', 'comment'=>'JSON serialized meta')
 			),
-			'primary'	=> array('id'),
-			'foreign'	=> array(
-				'bConfig_id'	=> null
-			)
+			'primary'	=> array('id')
 		)
 	)
 );

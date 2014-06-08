836ac727da5b55446cfa773e524e108a07dc9b9d
Spektr <spektrasm@gmail.com>
2014-06-09 00:09:18 +0400

    clear test item config

--- b/bIndex/__bConfig/_item/bIndex__bConfig_item.php
+++ b/bIndex/__bConfig/_item/bIndex__bConfig_item.php
@@ -3,7 +3,5 @@ defined('_BLIB') or die();
 return array(
 	array("name"=>"'{keywords}'", "value"=>""),
 	array("name"=>"'{description}'", "value"=>""),
-	array("name"=>"'{title}'", "value"=>""),
-	array("named"=>"'{title}'", "vsalue"=>""),
-	array("named"=>"'{title}'", "vsalue"=>"")
+	array("name"=>"'{title}'", "value"=>"")
 );

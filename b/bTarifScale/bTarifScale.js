59faf331a86a6221e0836db6477f0be6a169efa5
Spektr <spektr1987@rambler.ru>
2014-07-07 15:35:16 +0400

    correct showing bTarifScale

--- b/bTarifScale/bTarifScale.js
+++ b/bTarifScale/bTarifScale.js
@@ -37,6 +37,16 @@
 				});
 			}
 			
+			temp.push({
+				'tag':'tr',
+				'content':[
+					{'tag':'td', 'attrs':{'colspan':header.length}, 'content':[
+						{'elem':'regdata', 'content':'regdata'},
+						{'elem':'submit', 'content':'отправить'}
+					]}
+				]
+			});
+			
 			this.template = blib.clone(this.template);
 			this.template.mods = data.mods;
 			this.template.header = header;
@@ -95,19 +105,15 @@
 		},
 		//actions
 		{
-			'onclick':function(e){
-				console.log(e.blib);
-			}		
+				
 		}
 	);
 	
 	blib.build.define(
 		{'block':'bTarifScale', 'elem':'checker'},
 		function(data){
-			
-			//console.log(temp);
+			this.chousen = false;
 			this.template = blib.clone(this.template);
-			this.template.content = temp;
 		},
 		//template
 		{
@@ -117,7 +123,52 @@
 		//actions
 		{
 			'onclick':function(e){
-				console.log(e.blib);
+				var self = e.blib;
+				self.chousen = !self.chousen;
+			}		
+		}
+	);
+	
+	blib.build.define(
+		{'block':'bTarifScale', 'elem':'regdata'},
+		function(data){
+			this.template = blib.clone(this.template);
+			this.template.content = [
+				{'elem':'regHeader', 'content':"Для оформления заявки укажите персональные данные, чтобы мы могли связаться с Вами."},
+				{'elem':'regField', 'tag':'input', 'attrs':{'type':'text', 'name':'name', 'placeholder':"Имя", 'value':"Имя"}},
+				{'elem':'regField', 'tag':'input', 'attrs':{'type':'text', 'name':'email', 'placeholder':"Электронная почта", 'value':"Электронная почта"}},
+				{'elem':'regField', 'tag':'input', 'attrs':{'type':'text', 'name':'phone', 'placeholder':"Телефон", 'value':"Телефон"}},
+				{'elem':'regField', 'tag':'input', 'attrs':{'type':'text', 'name':'passport', 'placeholder':"Паспорт", 'value':"Паспорт"}},
+				{'elem':'regField', 'tag':'textarea', 'attrs':{'name':'passport_issued', 'placeholder':"Дата выдачи паспорта"}, 'content':"Дата выдачи паспорта"},
+				{'elem':'regField', 'tag':'textarea', 'attrs':{'name':'address', 'placeholder':"Адрес"}, 'content':"Адрес"}
+			];
+		},
+		false,
+		//actions
+		{
+			'onSetMode':{
+				'position':{
+					'horizontal':function(){
+						console.log('position -> horizontal');
+					},
+					'vertical':function(){
+						console.log('position -> vertical');
+					}
+				}
+			}		
+		}
+	);
+	
+	blib.build.define(
+		{'block':'bTarifScale', 'elem':'submit'},
+		function(data){
+			this.template.content = data.content;
+		},
+		false,
+		//actions
+		{
+			'onclick':function(e){
+				console.log('отправляем нахрен', e.blib.block.children.bTarifScale__checker);
 			}		
 		}
 	);

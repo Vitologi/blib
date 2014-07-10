cd4cac12006167289bbb3a6896ac6f2ecd9094c3
Spektr <spektr1987@rambler.ru>
2014-07-10 17:57:16 +0400

    start create bDocumentation

---
+++ b/bDocumentation/__bDatabase/_install/bDocumentation__bDatabase_install.php
@@ -0,0 +1,39 @@
+<?php
+defined('_BLIB') or die();
+return array(
+	'create'	=> array(
+		'bDocumentation'	=> array(
+			'fields' => array(
+				'id'		=> array('type'=> 'INT(10) UNSIGNED', 'null'=>'NOT NULL', 'extra'=> 'AUTO_INCREMENT', 'comment'=>'Table for store menu'),
+				'menu' => array('type'=> 'INT(10) UNSIGNED', 'comment'=>'menu group'),
+				'name'	=> array('type'=> 'VARCHAR(45)', 'null'=>'NOT NULL', 'comment'=>'item name'),
+				'link'	=> array('type'=> 'TEXT', 'null'=>'NULL', 'comment'=>'link view'),
+				'bConfig_id' => array('type'=> 'INT(10) UNSIGNED', 'comment'=>'config'),
+				'bMenu_id'	=> array('type'=> 'INT(10) UNSIGNED', 'comment'=>'arrow to parent')				
+			),
+			'primary'	=> array('id'),
+			'foreign'	=> array(
+				'bMenu_id'	=> null,
+				'bConfig_id'	=> null
+				
+			)
+		)
+	),
+	'insert' => array(
+		'bDocumentation' => array(
+			array('id', 'menu', 'name', 'link', 'bMenu_id'),
+			array('1', '1', 'main'),
+			array('2', '1', 'Home', '/', '1'),
+			array('3', '1', 'Documentation', null, '1'),
+			array('4', '1', 'Base', '/documentation/base/', '3'),
+			array('5', '1', 'API', null, '3'),
+			array('6', '1', 'Backend', '/documentation/api/backend/', '5'),
+			array('7', '1', 'Frontend', '/documentation/api/frontend/', '5'),
+			array('8', '1', 'FAQ', '/documentation/faq/', '3'),
+			array('9', '1', 'Downloads', '/downloads/', '1'),
+			array('10', '1', 'Analitics', null, '1'),
+			array('11', '1', 'Grafics', '/analitics/grafics/', '10'),
+			array('12', '1', 'Examples', '/analitics/examples/', '10')
+		)
+	)
+);
---
+++ b/bDocumentation/bDocumentation.php
@@ -0,0 +1,43 @@
+<?php
+defined('_BLIB') or die;
+
+class bDocumentation extends bBlib{	
+	
+	protected function inputSelf(){
+		$this->version = '1.0.0';
+		$this->parents = array('bSystem', 'bDatabase', 'bConfig');
+	}
+	
+	protected function input($data, $caller){
+		$this->data = $this->hook('getData', array($data));
+	}
+	
+	public function output(){
+	
+		if(!$this->data['menu']){return array();}
+		
+		$Q = array(
+			'select'	=> array(
+				'bMenu' => array('id', 'name', 'link' ,'bConfig_id' ,'bMenu_id')
+			),
+			'where' => array(
+				'bMenu' => array('menu'=>$this->data['menu'])
+			)
+		);
+		$result = $this->_query($Q);
+		
+		$menu = array();
+		
+		while($row = $result->fetch()){
+			if($row['bConfig_id']){$config = $this->_getConfig($row['bConfig_id']);}
+			$menu[] = array('id'=>$row['id'], 'name'=>$row['name'], 'config'=>$config, 'link'=>$row['link'], 'parent'=>$row['bMenu_id']);
+		}
+
+		return array('block'=>__class__, 'mods'=>$this->data['mods'], 'content'=>$menu, 'id'=>$this->data['menu']);
+	}
+	
+	protected function getData($data){
+		return $data;
+	}
+	
+}

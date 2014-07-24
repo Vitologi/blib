9ea95a536abba1cb34a0d0e577096e971237ee36
Spektr <spektr1987@rambler.ru>
2014-07-24 16:06:50 +0400

    correcting send data

--- b/bDocumentation/bDocumentation.js
+++ b/bDocumentation/bDocumentation.js
@@ -2,39 +2,110 @@
 	
 	blib.build.define(
 		{'block':'bDocumentation'},
-		function(data){
-			var group = data.group;
-			delete data.group;
+		(function(){
+			var singleton = false;
 			
-			console.log(data);
-			this.id = data.id;
-			this.template = blib.clone(this.template);
-			this.template.content = [
-				{'elem':'sidebar', 'content':[{'elem':'navigation', 'content':group}]},
-				{'elem':'content', 'content':[
-					{
-						'elem':'description',
-						'content': data
-					}
-				]}
-			];
+			
+			return function(data){
+				var group = data.group,
+					item = data.item;
+				
+				if(singleton){
+					if(group)singleton.setGroup(group);
+					if(item)singleton.setItem(item);
+				}else{
+					singleton = this;
+					this.template = blib.clone(this.template);
+					this.template.content = [
+						{'elem':'group', 'content':group},
+						{'elem':'item', 'content':item}
+					];
+				}
+			};			
+		})(),
+		false,
+		{
+			'setGroup':function(elem){
+				this.children.bDocumentation__group[0]._replace(elem);
+			},
+			'setItem':function(elem){
+				this.children.bDocumentation__item[0]._replace(elem);
+			},
+		}
+		
+	);
+	
+	blib.build.define(
+		{'block':'bDocumentation', 'elem':'group'},
+		function(data){
+			this.template = blib.clone(this.template);	
+			this.id = data.content.start;
+			this.navigation = {};
+			
+			var navigation = data.content.navigation;
+			for(key in navigation){
+				if(!this.navigation[navigation[key].group])this.navigation[navigation[key].group]=[];
+				this.navigation[navigation[key].group].push(navigation[key]);
+			}
+			
+			this.template.content = [this.getChild(this.id)];
+		},
+		false,
+		{
+			'getChild':function(id){
+				if(!id)return {};
+				var nav = this.navigation[id],
+					content = [],
+					temp;
+				
+				for(key in nav){
+					temp = {'elem':'li', 'tag':'li', 'content':[
+						{'elem':'opener', 'content':"+"},
+						{'elem':'link', 'item':nav[key].id, 'content':nav[key].name}
+					]};
+					temp.content.push(this.getChild(nav[key].id));
+					content.push(temp);
+				}
+				
+				return {'elem':'ul', 'tag':'ul', 'content':content};
+			}
 		}
 	);
 	
 	blib.build.define(
-		{'block':'bDocumentation', 'elem':'navigation'},
+		{'block':'bDocumentation', 'elem':'item'},
 		function(data){
-		console.log(data);
+			//console.log(data);
+		}
+	);
+	
+	blib.build.define(
+		{'block':'bDocumentation', 'elem':'link'},
+		function(data){
+			this.item = data.item;
 			this.template = blib.clone(this.template);	
-			this.id = data.content.start;
-			
-			var temp=[];
-			for(key in data.content.navigation){
-				
+			this.template.content = data.content;
+		},
+		{
+			'tag':'a',
+			'attrs':{
+				'href':'#'
 			}
-			
-			
-			this.template.content=[];
+		},
+		{
+			'onclick':function(){
+				var self = this;
+				blib.ajax({
+					'data':{'blib':'bDocumentation', 'id':this.item, 'ajax':true},
+					'success':function(data){
+						var item = {
+							'elem':'item',
+							'content':data.item
+						};						
+						self.block.setItem(item);
+					}
+				});				
+			}
 		}
 	);
 

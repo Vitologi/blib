7bdfbb4f22e8c70bb556886ca72fc97e1a3b9d89
Spektr <spektr1987@rambler.ru>
2014-09-09 18:19:47 +0400

    [bPanel] change block-link system (associate with bLink)

--- b/bPanel/bPanel.js
+++ b/bPanel/bPanel.js
@@ -12,46 +12,29 @@
 	blib.build.define(
 		{'block':'bPanel', 'elem':'blocks'},
 		function(data){
-			var content = [];
+			var content = [],
+				tunnel;
 			
 			for(key in data.content){
-				content.push({'block':'bPanel', 'elem':'blockLink', 'content':key});
+				tunnel = {'bPanel':{'controller':key}};
+				tunnel[key] = {'layout':"show"};
+				
+				content.push({
+					'block':'bLink',
+					'mods':{'style':'button', 'position':'vertical'},
+					'ajax':true,
+					'tunnel':tunnel,
+					'content':key,
+					'before':[function(){
+						this.data = {'template':blib('.bTemplate')[0].blib.template.template};
+					}]
+				});
 			}
+			
 			this.template.content = content;
 
 		}
 	);
+
 	
-	blib.build.define(
-		{'block':'bPanel', 'elem':'blockLink'},
-		function(data){
-			this.link = data.content;
-			this.template = data;
-
-		},
-		{"tag":"a"},
-		{
-			'onclick':function(){
-				var self = this,
-					tunnel = {'bPanel':{'controller':self.link}},
-					template;
-					
-				tunnel[self.link] = {'layout':"show"};
-				template = blib('.bTemplate')[0].blib.template.template;
-				
-				blib
-				.tunnel(tunnel)
-				.ajax({
-					'url':window.location,
-					'data':{'ajax':true, 'template':template},
-					'dataType':'json',
-					'success':function(data){
-						blib('body').html(blib.build(data));
-					}
-				});
-				
-			}
-		}
-	);
-
 })(window);

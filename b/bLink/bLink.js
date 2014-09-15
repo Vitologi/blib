78acaef5b9a15a47f047953c4591e51d2035476b
Spektr <spektr1987@rambler.ru>
2014-09-15 18:49:52 +0400

    [bLink] add uphold object like table or form, error element for show message

--- b/bLink/bLink.js
+++ b/bLink/bLink.js
@@ -1,8 +1,9 @@
 (function(){
 	
 	var config = {
-		'curloc':window.location.pathname
-	};
+			'curloc':window.location.pathname
+		},
+		storage = {};
 	
 	Blib.config("bLink", config);
 	//Blib.config("_private.bLink", true); //0_0 open until bMenu is not based on bLink
@@ -15,6 +16,7 @@
 			this.tunnel = data.tunnel;
 			this.data = data.data;
 			this.disabled = (data.mods)?data.mods.disabled:false;
+			this.uphold = data.uphold || [];
 			this.before = data.before || [];
 			this.after = data.after || [];
 			this.invisible = data.invisible || false;
@@ -22,12 +24,42 @@
 			
 			this.template.attrs.href = this.link;
 			this.template.mods = data.mods || {};
-			this.template.content = data.content;
+			this.template.content = [
+				{'elem':'error'},
+				{'tag':'span', 'content':data.content}				
+			];
 		},
 		{
 			'tag':'a'
 		},
-		{
+		{	
+			'getUpholdStatus':function(){
+				var self = this,
+					result = {'error':0, 'code':0, 'message':'', 'stack':[]},
+					uphold = self.uphold,
+					uLen = uphold.length,
+					i=0, j, temp, status, stack;
+				
+				for(;i<uLen;i++){
+					temp = storage[uphold[i]];
+					if(!temp || !('getStatus' in  temp))continue;
+					
+					status = temp.getStatus();
+					stack = result.stack;
+					if(status.error)result = blib.extend({'error':0, 'code':0, 'message':'', 'stack':[]}, status);
+					stack.push(status);
+					result.stack = stack;
+				}
+				
+				return result;
+				
+			},
+			'setUphold':function(name, obj){
+				storage[name] = obj;
+			},
+			'dropUphold':function(name, obj){
+				if(storage[name] === obj)delete storage[name];
+			},
 			'getCurloc':function(){return config.curloc;},
 			'setCurloc':function(data){config.curloc = data;},
 			
@@ -37,9 +69,13 @@
 				var self = this,
 					before = self.before,
 					after = self.after,
-					key;
+					allow, key;
+				
 				
 				if(self.disabled)return false;
+				allow = self.getUpholdStatus();
+				if(allow.error)return self.showError(allow);
+				self.clearError();
 				
 				for(key in before){
 					before[key].call(self);
@@ -90,6 +126,34 @@
 					window.chHashFlag = true;
 					location.hash = self.getCurloc();
 				}
+			},
+			'showError':function(error){
+				var elem = (this.children && this.children.bLink__error)?this.children.bLink__error[0]:false;
+				if(elem)elem.show(error);
+			},
+			'clearError':function(){
+				var elem = (this.children && this.children.bLink__error)?this.children.bLink__error[0]:false;
+				if(elem)elem.clear();
+			}
+		}
+	);
+	
+	blib.build.define(
+		{'block':'bLink', 'elem':'error'},
+		function(data){
+			this.template = data;
+		},
+		{'tag':'span'},
+		{	
+			'show':function(error){
+				var message = error.message;
+				if(!blib.is(message,"object"))message = {'tag':'span', 'content':message};
+				this._append(message,true);
+				this._setMode('active',true);
+			},
+			'clear':function(){
+				this._removeChildren();
+				this._setMode('active',false);
 			}
 		}
 	);

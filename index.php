83bb46e4fd38dc5540f2b60d194e99220ef2e754
Spektr <spektr1987@rambler.ru>
2015-01-27 16:58:07 +0300

    clear index file

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 	error_reporting(-1);
 	if(!array_key_exists('blib', $_GET)){$_GET['blib']='bIndex';}
-	//$vremiya_starta = microtime(true);
  
 	require_once("b/bBlib/bBlib.php");
-	bBlib::gate();
-	
-
-	//$vremya_okonchaniya = microtime(true);
-	//$vremya = $vremya_okonchaniya - $vremiya_starta;
-	//$vremya = round($vremya, 2);
-	 
-	//print "Время выполнения скрипта $vremya секунд(ы)...";
+	bBlib::init($_GET['blib']);

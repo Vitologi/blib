0b55c3721afae50a7cbc88b0ac7ad79252e92fdd
Spektr <spektr1987@rambler.ru>
2015-02-02 13:01:32 +0300

    [bSystem] change arguments in _default function

--- b/bSystem/bSystem.php
+++ b/bSystem/bSystem.php
@@ -4,8 +4,8 @@ defined('_BLIB') or die;
 class bSystem extends bBlib{
     
     // Set default value for block
-    public static function _default($data, $parent){
-        list($key, $value, $default) = $data;
+    public static function _default($key, $value, $default, $parent){
+
         if(is_string($key)){
             $parent->$key = ($value?$value:$default);
         }

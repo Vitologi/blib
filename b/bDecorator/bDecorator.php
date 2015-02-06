f8a8713f9871039c76f5edae664145e65962ea63
Spektr <spektr1987@rambler.ru>
2015-02-06 15:42:41 +0300

    [bDecorator] include configuration

--- b/bDecorator/bDecorator.php
+++ b/bDecorator/bDecorator.php
@@ -1,40 +1,39 @@
 <?php
 defined('_BLIB') or die;
 
-class bDecorator extends bBlib{	
-	
+class bDecorator extends bBlib{
+
     private static $_instance = null;
-    private $_list = array();
+    private        $_list     = array();
+    protected      $_traits   = array('bSystem', 'bConfig');
     
     
     // Overload object factory for Singleton
     static public function create() {
         if (self::$_instance === null)self::$_instance = parent::create(func_get_args());
+        self::$_instance->_parent = null;
         return self::$_instance;
     }
      
     protected function input(){
-        $path = bBlib::path('bDecorator', 'ini');
-        if(!file_exists($path)){return;}
-        $this->_list = json_decode(file_get_contents($path), true);
+        $this->_list = $this->_getConfig();
     }
     
 	public function output(){
         $block  = get_class($this->_parent);
-        
+
         if( isset($this->_list[$block]) && $list = $this->_list[$block]){
-                
 			foreach( $list as $key=>$decorator){
 				$this->_parent = $decorator::create()->setParent($this->_parent);
 			}
 		}
-        
+
         return $this->_parent;
         
 	}
 
     
-    final public static function _decorate($data, $caller){
+    final public static function _decorate(bBlib $caller){
         return $caller->getInstance('bDecorator');
 	}
     
@@ -43,7 +42,7 @@ class bDecorator extends bBlib{
 	}
     
     final public function setList($list){
-       file_put_contents(bBlib::path('bDecorator', 'ini'), json_encode($list));
+       $this->_setConfig($list);
 	}
     
 }

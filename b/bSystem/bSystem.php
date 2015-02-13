fec62fd7b10b28e60c008f9e707b11203c03c4a7
Spektr <spektr1987@rambler.ru>
2015-02-13 17:20:23 +0300

    [bSystem] add function for work with object with dotted notation

--- b/bSystem/bSystem.php
+++ b/bSystem/bSystem.php
@@ -2,7 +2,7 @@
 defined('_BLIB') or die;
 
 class bSystem extends bBlib{
-    
+
     // Set default value for block
     public static function _default($key, $value, $default, $parent){
 
@@ -12,5 +12,42 @@ class bSystem extends bBlib{
         return $parent;
         
     }
-    
+
+    // Navigate in array by dotted notation
+    public static function _navigate(){
+        if(func_num_args()===4) {
+            list($obj, $selector, $value, $parent) = func_get_args();
+            $isValue = true;
+        }else if(func_num_args()===3){
+            list($obj, $selector, $parent) = func_get_args();
+            $isValue = false;
+        }else{
+            throw new Exception('Wrong function arguments');
+        }
+
+        if(!is_array($obj) || !is_string($selector)) return null;
+
+        $temp = &$obj;
+        $needle = explode('.',$selector);
+        $i=0;
+        $len=count($needle);
+
+        while($i<$len){
+            if(!is_array($temp) || $isValue and $i == $len-1) break;
+
+            if(!isset($temp[$needle[$i]])){
+                if(!$isValue)return null;
+                $temp[$needle[$i]] = array();
+            }
+
+            $temp = &$temp[$needle[$i]];
+            $i++;
+        };
+
+        if(!$isValue) return $temp;
+        $temp[$needle[$i]] = $value;
+
+        return $obj;
+    }
+
 }

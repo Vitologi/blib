0130729a6b3455e75c0dc62b682cc1d4434cd2e2
Spektr <spektr1987@rambler.ru>
2015-03-10 15:48:39 +0300

    [bDocumentation] refactor server side

--- b/bDocumentation/bDocumentation.php
+++ b/bDocumentation/bDocumentation.php
@@ -1,78 +1,58 @@
 <?php
 defined('_BLIB') or die;
 
-class bDocumentation extends bBlib{	
-	
-	protected function inputSelf(){
-		$this->version = '1.0.0';
-		$this->parents = array('bSystem', 'bDatabase', 'bConfig');
-	}
-	
-	protected function input($data, $caller){
-		$this->data = $this->hook('getData', array($data));
-	}
-	
+class bDocumentation extends bBlib{
+
+    protected $_traits   = array('bSystem', 'bConfig', 'bDataMapper', 'bDecorator', 'bRequest');
+
+    /** @var array $_template - base template for frontend */
+    private $_template = array(
+        'block'      => __class__,
+        'mods'       => array(),
+        'content'    => array(),
+        'id'         => null,
+        'item'       => null,
+        'chapter'    => null,
+        'navigation' => array()
+    );
+
+
+	 protected function input($template = array()){
+
+         /** @var bDecorator__instance $bDecorator - decorator instance */
+         $bDecorator = $this->getInstance('bDecorator');
+
+         /** @var bRequest $bRequest - request instance */
+         $bRequest = $this->getInstance('bRequest');
+
+
+         // Decor template
+         $decorTemplate = $bDecorator->getData($template);
+         $requestTemplate =($bRequest->get('blib')===__CLASS__)?array('id'=>$bRequest->get('id'), 'ajax'=>$bRequest->get('ajax')):array();
+         $this->_template = array_replace_recursive($this->_template, $decorTemplate, $requestTemplate);
+    }
+
 	public function output(){
 
-		$answer = array(
-			'block' => __class__,
-			'mods' => isset($this->data['mods'])?$this->data['mods']:array()
-		);
+        $itemId = $this->_template['id'];
+        $chapterId = $this->_template['chapter'];
 
-		if(isset($this->data['id'])){
-		
-			$Q = array(
-				'select'	=> array(
-					'bdocumentation' => array('id', 'name', 'description' ,'group', 'parent'=>'bdocumentation_id')
-				),
-				'where' => array(
-					'bdocumentation' => array('id'=>$this->data['id'])
-				)
-			);
-			$result = $this->_query($Q);
-			$temp = $result->fetch(PDO::FETCH_ASSOC);
-			$temp['description'] = json_decode($temp['description']);
-			$group = json_decode($temp['group']);
-			$content = array();
-			unset($temp['group']);
-			
-			if($group){
-				foreach($group as $id){
-					$where[] = array('id',$id,false,true);
-				}
-				$Q = array(
-					'select'	=> array(
-						'bdocumentation' => array('id', 'name', 'note', 'parent'=>'bdocumentation_id')
-					),
-					'where' => array(
-						'bdocumentation' => $where
-					)
-				);
-				$result = $this->_query($Q);
-				$content = $result->fetchALL(PDO::FETCH_ASSOC);
-			}
-			
-			$temp['content'] = $content;
-			$answer['item'] = $temp;
-		}
-		
-		if(isset($this->data['chapter'])){
-			$Q = array(
-				'select'	=> array(
-					'bdocumentation' => array('id', 'name', 'parent'=>'bdocumentation_id')
-				)
-			);
-			$result = $this->_query($Q);
-			$navigation = $result->fetchALL(PDO::FETCH_ASSOC);
-						
-			$answer['navigation'] = $navigation;
-			$answer['chapter'] = $this->data['chapter'];
-		}
-		
-		if(isset($this->data['ajax'])){
-			echo json_encode($answer);
+
+        if($itemId !== null){
+            $item = $this->getItem($itemId);
+            $this->_template['item'] = $item;
+        }
+
+        if($chapterId !== null){
+            $navigation = $this->getNavigation($chapterId);
+            $this->_template['navigation']  = $navigation;
+        }
+
+
+		if(isset($this->_template['ajax'])){
+			echo json_encode($this->_template, 256);
 		}else{
-			return $answer;
+			return $this->_template;
 		}
 		
 	}
@@ -80,5 +60,38 @@ class bDocumentation extends bBlib{
 	protected function getData($data){
 		return $data;
 	}
-	
+
+    /**
+     * Get documentation from database
+     *
+     * @param null|int $id  - item id
+     * @return array        - all menu items included in menu
+     */
+    protected function getItem($id = null){
+
+        if(!$id){return array();}
+
+        /** @var bDocumentation__bDataMapper $bDataMapper - data mapper instance */
+        $bDataMapper = $this->getInstance('bDataMapper');
+
+        // get menu item list
+        $item = $bDataMapper->getItem($id);
+
+        $item->content = ($item->group)?$bDataMapper->getList($item->group):array();
+
+        return  $item;
+
+    }
+
+    protected function getNavigation(){
+
+        /** @var bDocumentation__bDataMapper $bDataMapper - data mapper instance */
+        $bDataMapper = $this->getInstance('bDataMapper');
+
+        // get menu item list
+        $navigation = $bDataMapper->getList();
+
+        return $navigation;
+
+    }
 }

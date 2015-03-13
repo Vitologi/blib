e4c8493d00c150d1c9bf22ebe93133855d2ecee0
Spektr <spektr1987@rambler.ru>
2015-03-13 16:25:54 +0300

    [bConverter] create converter factory

--- b/bConverter/bConverter.php
+++ b/bConverter/bConverter.php
@@ -1,15 +1,25 @@
 <?php
 defined('_BLIB') or die;
 
+/**
+ * Class bConverter     - for change type of data and display it correctly
+ *  Included patterns:
+ * 	  singleton	    - one converter factory
+ * 	  decorator 	- many rules of format change
+ */
 class bConverter extends bBlib{
 
     /** @var null|static $_instance - Singleton instance */
-    private static $_instance = null;
+    private static $_instance   = null;
 
-    protected $_traits     = array('bSystem');
-    protected $_converters = array('bConverter__instance');
-    private   $_data       = null;
-    private   $_format     = null;
+    /** @var null|bConverter__instance  $_converter */
+    private        $_converter  = null;
+    /**
+     * @var array $_converters  - list of convert rules
+     */
+    protected      $_converters = array('bConverter__default','bConverter__json');
+
+    protected      $_traits     = array('bSystem');
 
     /**
      * Overload object factory for Singleton
@@ -17,59 +27,34 @@ class bConverter extends bBlib{
      * @return bConverter|null|static
      */
     static public function create() {
-        if (self::$_instance === null)parent::create(func_get_args());
+        if (self::$_instance === null)self::$_instance = parent::create(func_get_args());
         return self::$_instance;
     }
 
-    protected function input($data = null){
-        $this->_data = $data;
-        $decorConverter = $this;
+    /**
+     *  Create decorated converter
+     */
+    protected function input(){
+
+        /** @var  bConverter__instance $decorConverter */
+        $decorConverter = bConverter__instance::create()->setParent($this);
 
         foreach($this->_converters as $converter){
 
             /** @var bConverter__instance $converter - decor Class */
-            $decorConverter = $converter::create()->setParent($decorConverter);
+            $decorConverter = $converter::create()->setParent($this)->decor($decorConverter);
 
         }
 
-        self::$_instance = $decorConverter;
+        $this->_converter = $decorConverter;
     }
 
-
-    final public function getData(){
-        return $this->_data;
-    }
-
-    final public function setData($data = null){
-        $this->_data = $data;
-        return $this;
-    }
-
-
-    final public function setFormat($format = null){
-        if(is_string($format)){
-            $this->_format = $format;
-        }
-
-        return $this;
-    }
-
-    final public function getFormat(){
-        return $this->_format;
-    }
-
-
-    final  public function convertTo($newFormat = null){
-        if($this->_format === null)$this->getFormat();
-        return $this->_data;
-    }
-
+    /**
+     * Return decorated converter instead self
+     *
+     * @return bConverter__instance|null    - converter
+     */
     public function output(){
-        return ($this->_parent?$this:$this->view());
-    }
-
-    public function view(){
-        return null;
+        return $this->_converter;
     }
 }
-

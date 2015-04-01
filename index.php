08f37facdeefdd3a0aa96e099222cd1cf4642296
Spektr <spektr1987@rambler.ru>
2015-04-01 16:55:04 +0300

    [vendor] add foreign vendors to project (validator)

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+	require_once("vendor/autoload.php");
+
 	error_reporting(-1);
     $request = (array)json_decode(file_get_contents("php://input"),true)+(array)$_POST +(array)$_GET;
 	if(!isset($request['blib'])){ $request['blib']='bIndex';}
- 
+
 	require_once("b/bBlib/bBlib.php");
 	bBlib::init($request['blib']);
